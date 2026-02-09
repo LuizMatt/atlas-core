52fa442b1857242aca183fc89e9a5b58c30f89d5
LuizMatt <luizmatheuslins5587@gmail.com>
2026-02-09 11:34:35 -0300

    chore: adjusting primevue configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  compatibilityDate: "2025-07-15",
+
+  css: ["primeicons/primeicons.css"],
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxtjs/i18n',
-    '@nuxtjs/seo',
-    '@nuxtjs/tailwindcss',
-    '@primevue/nuxt-module'
-  ]
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxtjs/i18n",
+    "@nuxtjs/seo",
+    "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
+  ],
+
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: "filled",
+      theme: {
+        preset: Aura,  
+        options: {
+          prefix: "p",
+          darkModeSelector: "system",
+          cssLayer: false,
+        },
+      },
+    },
+  },
 })
